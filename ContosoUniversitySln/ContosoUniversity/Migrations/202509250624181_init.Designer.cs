862bde6f421dace684ccc88d670078dedf3cc08e
Sornali-Sanu <sornali.sanu@gmail.com>
2025-09-25 12:37:18 +0600

    new model and seed data added

--- ContosoUniversitySln/ContosoUniversity/Migrations/202509250624181_init.Designer.cs
+++ ContosoUniversitySln/ContosoUniversity/Migrations/202509250624181_init.Designer.cs
@@ -13,7 +13,7 @@ namespace ContosoUniversity.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202509250438444_init"; }
+            get { return "202509250624181_init"; }
         }
         
         string IMigrationMetadata.Source
